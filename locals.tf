2722356f6ed76f64116c16cddf285b97216e03ee
vineeth kumar milatari <milatariv@mail.sacredheart.edu>
2026-03-03 10:23:30 -0500

    actions

---
+++ locals.tf
@@ -0,0 +1,17 @@
+locals {
+    common_tags ={
+        project = var.project
+        Environment = var.environment
+        Terraform = "true"
+    }
+
+    vpc_final_tags =  merg (
+        local.common_tags,
+        {
+        name = "${var.project}-${var.environment }"
+        },
+        var.vpc_tags 
+
+  )
+
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "project" {
+    type= string
+  
+}
+
+variable "environment" {
+    type= string
+  
+}
+
+variable "vpc_cidr" {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+
+variable "vpc_tags" {
+    type = map
+    default = {}
+  
+}
