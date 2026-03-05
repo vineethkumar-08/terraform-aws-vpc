2ed99e09f2dad7f6bb2791ea0a5515542e5dc956
vineeth kumar milatari <milatariv@mail.sacredheart.edu>
2026-03-05 15:26:02 -0500

    actions

--- main.tf
+++ main.tf
@@ -12,6 +12,8 @@ resource "aws_internet_gateway" "main" {
   tags = local.igw_final_tags
 }
 
+
+
 resource "aws_subnet" "public" {
     count= length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
@@ -30,4 +32,95 @@ resource "aws_subnet" "public" {
       
     )
     
+}
+
+
+resource "aws_subnet" "private" {
+    count= length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+        local.common_tags, 
+        # roboshop-dev-private-is-east-1a
+        {
+         Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+
+        },
+        var.private_subnet_tags
+      
+    )
+    
+}
+
+resource "aws_subnet" "database" {
+    count= length(var.database_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+        local.common_tags, 
+        # roboshop-dev-private-is-east-1a
+        {
+         Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
+
+        },
+        var.database_subnet_tags
+      
+    )
+    
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example.id
+tags = merge(
+        local.common_tags, 
+        # roboshop-dev-private-is-east-1a
+        {
+         Name = "${var.project}-${var.environment}-public" 
+
+        },
+        var.public-route_table_tags
+      
+    )
+  
+
+  
+}
+
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.example.id
+tags = merge(
+        local.common_tags, 
+        # roboshop-dev-private-is-east-1a
+        {
+         Name = "${var.project}-${var.environment}-private" 
+
+        },
+        var.private-route_table_tags
+      
+    )
+  
+
+  
+}
+
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.example.id
+tags = merge(
+        local.common_tags, 
+        # roboshop-dev-database-is-east-1a
+        {
+         Name = "${var.project}-${var.environment}-database" 
+
+        },
+        var.database-route_table_tags
+      
+    )
+  
+
+  
 }
--- variables.tf
+++ variables.tf
@@ -36,3 +36,43 @@ variable "public_subnet_tags" {
     type = map
     default = {}
 }
+
+variable "private_subnet_cidrs"{
+    type= list
+    default = ["10.0.11.0/24","10.0.22.0/24"]
+}
+
+variable "private_subnet_tags" {
+    type = map
+    default = {}
+}
+
+variable "database_subnet_cidrs"{
+    type= list
+    default = ["10.0.11.0/24","10.0.22.0/24"]
+}
+
+variable "database_subnet_tags" {
+    type = map
+    default = {}
+}
+
+
+
+variable "public-route_table_tags" {
+  type = map
+  default = {}
+  
+}
+
+variable "private-route_table_tags" {
+  type = map
+  default = {}
+  
+}
+
+variable "database-route_table_tags" {
+  type = map
+  default = {}
+  
+}
