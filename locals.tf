4b49a4c914a91858abce249ed332781c4d733553
vineeth kumar milatari <milatariv@mail.sacredheart.edu>
2026-03-06 02:13:36 -0500

    action

--- locals.tf
+++ locals.tf
@@ -1,30 +1,23 @@
 locals {
-    common_tags ={
-        project = var.project
+    common_tags = {
+        Project = var.project
         Environment = var.environment
         Terraform = "true"
     }
-
-    vpc_final_tags =  merge (
+    vpc_final_tags = merge(
         local.common_tags,
         {
-        name = "${var.project}-${var.environment }"
+            Name = "${var.project}-${var.environment}"
         },
-        var.vpc_tags 
-
-  )
-
-  igw_final_tags = merge(
-    local.common_tags,
-    {
-        name = "${var.project}-${var.environment }"
-    },
-    var.igw_tags
-    
+        var.vpc_tags
     )
-
-    az_names = slice(data.aws_availability_zones.available.names, 0,2)
-
-
-
+    igw_final_tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}"
+        },
+        var.igw_tags
+    )
+    az_names = slice(data.aws_availability_zones.available.names, 0, 2)
+    
 }
--- variables.tf
+++ variables.tf
@@ -1,89 +1,86 @@
 variable "project" {
-  type = string
-
+    type = string
 }
 
 variable "environment" {
-  type = string
-
+    type = string
+    validation {
+        condition     = contains(["dev", "qa", "uat", "prod"], var.environment)
+        error_message = "Environments should be one of dev, qa, uat or prod"
+    }
 }
 
 variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
+    type = string
+    default = "10.0.0.0/16"
 }
-
 
 variable "vpc_tags" {
-  type    = map(any)
-  default = {}
-
+    type = map
+    default = {}
 }
-
 
 variable "igw_tags" {
-  type    = map(any)
-  default = {}
+    type = map
+    default = {}
 }
 
-
-variable "public_subnet_cidrs"{
-    type= list
-    default = ["10.0.1.0/24","10.0.2.0/24"]
+variable "public_subnet_cidrs" {
+    type = list
+    default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "public_subnet_tags" {
-    type = map
     default = {}
+    type = map
 }
 
-variable "private_subnet_cidrs"{
-    type= list
-    default = ["10.0.11.0/24","10.0.22.0/24"]
+variable "private_subnet_cidrs" {
+    type = list
+    default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "private_subnet_tags" {
-    type = map
     default = {}
+    type = map
 }
 
-variable "database_subnet_cidrs"{
-    type= list
-    default = ["10.0.11.0/24","10.0.22.0/24"]
+variable "database_subnet_cidrs" {
+    type = list
+    default = ["10.0.21.0/24", "10.0.22.0/24"]
 }
 
 variable "database_subnet_tags" {
-    type = map
     default = {}
+    type = map
 }
 
-
-
-variable "public-route_table_tags" {
-  type = map
-  default = {}
-  
+variable "public_route_table_tags" {
+    default = {}
+    type = map
 }
 
-variable "private-route_table_tags" {
-  type = map
-  default = {}
-  
+variable "private_route_table_tags" {
+    default = {}
+    type = map
 }
 
-variable "database-route_table_tags" {
-  type = map
-  default = {}
-  
+variable "database_route_table_tags" {
+    default = {}
+    type = map
 }
+
 variable "eip_tags" {
-  type = map
-  default = {}
-  
+    default = {}
+    type = map
 }
 
 variable "nat_gateway_tags" {
-  type = map
-  default = {}
-  
+    default = {}
+    type = map
+}
+
+variable "is_peering_required" {
+    default = false
+    type = bool
 }
